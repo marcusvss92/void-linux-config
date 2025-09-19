e4777d297237807640ac2a12ed26d5db893922d3
Marcus Vinicius Salvador <119147574+marcusvss92@users.noreply.github.com>
2025-09-19 02:27:22 -0300

    Update update-efi-entry.sh
    Creating a new variable GRAPHICS_PARAMS and editing the script.

--- usr/local/bin/update-efi-entry.sh
+++ usr/local/bin/update-efi-entry.sh
@@ -7,8 +7,26 @@ ROOT_UUID=$(blkid -s UUID -o value /dev/mapper/voidlinux-root 2>/dev/null || blk
 # Security flag configurations
 HARDENING_FLAGS_FULL="slab_nomerge init_on_alloc=1 init_on_free=1 page_alloc.shuffle=1 pti=on vsyscall=none debugfs=off oops=panic lockdown=confidentiality mce=0 slub_debug=P page_poison=1"
 HARDENING_FLAGS_MINIMAL="pti=on vsyscall=none"
+
+# Specific parameteres for Lenovo LOQ + RTX 2050 + Wayland
+GRAPHICS_PARAMS="nvidia-drm.modeset=1 nvidia-drm.fbdev=1 nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+
+# For better hybrid support
+GRAPHICS_PARAMS+=" i915.force_probe=* i915.enable_psr=0"
+
+# NVIDIA flags
 NVIDIA_FLAGS="nvidia_drm.modeset=1"
+
+# No flags
 NO_FLAGS=""
+
+# Exemplo de entrada EFI atualizada:
+#efibootmgr --create \
+#    --disk /dev/sda \
+#    --part 1 \
+#    --label "Void Linux Wayland" \
+#    --loader /vmlinuz-${KERNEL_VERSION} \
+#    --unicode "root=UUID=${ROOT_UUID} ro ${HARDENING_FLAGS} ${GRAPHICS_PARAMS} ${HIBERNATION_PARAMS} initrd=\\initramfs-${KERNEL_VERSION}.img"
 
 # Check for hibernation configuration
 HIBERNATION_PARAMS=""
@@ -36,7 +54,7 @@ create_all_entries() {
         --part 1 \
         --label "Void Linux Secure" \
         --loader /vmlinuz-${KERNEL_VERSION} \
-        --unicode "root=UUID=${ROOT_UUID} ro quiet ${NVIDIA_FLAGS} ${HARDENING_FLAGS_FULL} ${HIBERNATION_PARAMS} initrd=\\initramfs-${KERNEL_VERSION}.img"
+        --unicode "root=UUID=${ROOT_UUID} ro quiet ${HARDENING_FLAGS_FULL} ${GRAPHICS_PARAMS} ${HIBERNATION_PARAMS} initrd=\\initramfs-${KERNEL_VERSION}.img"
 
     # Entry 2: Pentesting Compatible
     echo "Creating PENTEST entry..."
@@ -45,7 +63,7 @@ create_all_entries() {
         --part 1 \
         --label "Void Linux Pentest" \
         --loader /vmlinuz-${KERNEL_VERSION} \
-        --unicode "root=UUID=${ROOT_UUID} ro quiet ${NVIDIA_FLAGS} ${HARDENING_FLAGS_MINIMAL} ${HIBERNATION_PARAMS} initrd=\\initramfs-${KERNEL_VERSION}.img"
+        --unicode "root=UUID=${ROOT_UUID} ro quiet ${HARDENING_FLAGS_MINIMAL} ${GRAPHICS_PARAMS} ${HIBERNATION_PARAMS} initrd=\\initramfs-${KERNEL_VERSION}.img"
 
     # Entry 3: Maximum Compatibility
     echo "Creating COMPATIBLE entry..."
@@ -54,7 +72,7 @@ create_all_entries() {
         --part 1 \
         --label "Void Linux Compatible" \
         --loader /vmlinuz-${KERNEL_VERSION} \
-        --unicode "root=UUID=${ROOT_UUID} ro quiet ${NVIDIA_FLAGS} ${HIBERNATION_PARAMS} initrd=\\initramfs-${KERNEL_VERSION}.img"
+        --unicode "root=UUID=${ROOT_UUID} ro quiet ${GRAPHICS_PARAMS} ${HIBERNATION_PARAMS} initrd=\\initramfs-${KERNEL_VERSION}.img"
 
     echo "All EFI entries created:"
     efibootmgr | grep "Void Linux"
